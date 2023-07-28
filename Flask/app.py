9be60bb0efdaed51bb6453c9c2ee3cb7f8688f6a
PriscilaGlienke <priglienke@gmail.com>
2023-07-27 21:33:43 -0400

    updates

--- Flask/app.py
+++ Flask/app.py
@@ -17,14 +17,20 @@ def api_fraud_address():
     print(keys)
     return jsonify(output_data)
 
-@app.route("/api/fraud_personal")
-def api_fraud_personal():
-
-    df = pd.read_csv('Fraud_Personal.csv')
-    sample_perosnaldf = df.sample(n=10000)
-    personal_totals = sample_perosnaldf['gender']
-    output_personal = personal_totals.tolist()
-    return jsonify(output_personal)
+# @app.route('/api/fraud_address')
+# def map():
+#     # Create a map centered at the first entry's coordinates
+#     map_center = [data.iloc[0]['lat'], data.iloc[0]['long']]
+#     fraud_map = folium.Map(location=map_center, zoom_start=6)
+#     # Add markers for each fraud address
+#     for _, row in data.iterrows():
+#         popup_text = f"{row['first']} {row['last']}<br>{row['street']}<br>{row['city']}, {row['state']}<br>Population: {row['city_pop']}"
+#         folium.Marker(location=[row['lat'], row['long']], popup=popup_text).add_to(fraud_map)
+#     # Save the map to an HTML file
+#     map_file = "fraud_map.html"
+#     fraud_map.save(map_file)
+#     # Render the template with the map file path
+#     return render_template('index.html', map_file=map_file)
    
 @app.route("/api/Fraud_Merch")
 def api_Fraud_Merch():
