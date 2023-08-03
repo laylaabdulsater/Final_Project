50b82643122af3a23a92d3db5e89e9a929506365
laylaabdulsater <layla-abdulsater@hotmail.com>
2023-08-02 22:39:15 -0400

    Final

--- Flask/static/js/app.js
+++ Flask/static/js/app.js
@@ -1,36 +1,29 @@
-// Creating the map object
-let myMap;
-// Creating the marker cluster group
-let markers = L.markerClusterGroup();
-
-// Function to initialize the map
-function initializeMap() {
-  myMap = L.map("map", {
-    center: [37.0902, -95.7129], // Centered on the United States
-    zoom: 4 // Zoom level adjusted to show the entire country
-  });
-
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-}
-
 // Function to display the fraud density map
 function displayFraudDensityMap() {
   // Define the JSON URL
   let fraudAddressURL = '/api/fraud_address';
   // Load the JSON data using d3
   d3.json(fraudAddressURL).then(function(data) {
-    console.log(Object.keys(data))
+
     let latObj = data.lat;
     let longObj = data.long;
-    console.log(latObj)
-    console.log(longObj)
+
+    let myMap = L.map("map", {
+      center: [37.0902, -95.7129], // Centered on the United States
+      zoom: 4 // Zoom level adjusted to show the entire country
+    });
+  
+    // Adding the tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+
+    let markers = L.markerClusterGroup();
     for (let i = 0; i < 500000; i++) {
       let marker = L.marker([latObj[i], longObj[i]])
       markers.addLayer(marker)
     }
+
     myMap.addLayer(markers);
   }).catch(error => console.log("Error loading JSON data:", error));
 }
@@ -42,68 +35,66 @@ function displayPieChart(){
   let fraudMerchURL = '/api/Fraud_Merch';
 
   //Load the data
-  d3.json(fraudMerchURL).then(function(data){ 
+  d3.json(fraudMerchURL).then(function(data) { 
     //Parse the data
-    data.forEach(d=> {
-      d.amt= +d.amt;
+    data.forEach(d => {
+      d.amt = +d.amt;
     });
 
-    //Create D3 Pie Layout
-    pie = d3.pie()
-      .value(d=>d.amt)
-      .sort(null);
-
-    //Specify the colors for the pie slices
-    let colorScale = d3.scaleOrdinal()
-      .domain(data.map(d=>d.category))
-      .range(d3.schemeCategory10);
-
     //Select the char container element
-    let chartContainer = d3.select("#chart");
+    let chartContainer = d3.select("#pieChart");
 
     //Clear the existing chart
     chartContainer.html("");
 
     // Append the chart to the chart container
     let svg = chartContainer.append("svg")
-      .attr("width", 800)
-      .attr("height", 800)
-      .append("g")
-      .attr("transform", "translate (400, 400)");
-    
-    //Generate the pie slices
-    let arcs = svg.selectAll("arc")
-      .data(pie(data))
-      .enter()
-      .append("g");
+        .attr("width", 500)
+        .attr("height", 400)
 
-    //Add the pie slices as pie elements
-    arcs.append("path")
-      .attr("d", d3.arc().innerRadius(0).outerRadius(150))
-      .attr("fill", d=> colorScale(d.piedata.category))
-      .attr("stroke", "white")
-      .style("stroke-width", "2px");
+    var width = svg.attr("width"),
+        height = svg.attr("height"),
+        radius = 200;
+       
+        var g = svg.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    //Add legend
-    let legend = svg.selectAll(".legend")
-      .data(data)
-      .enter()
-      .append("g")
-      .attr("class", "legend")
-      .attr("transform", (d, i) => `translate (250,${i*25})`);
+// Step 4
+var ordScale = d3.scaleOrdinal()
+               .domain(data)
+               .range(['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72']);
 
-    legend.append("rect")
-      .attr("x", -18)
-      .attr("y", -10)
-      .attr("width", 10)
-      .attr("height", 10)
-      .attr("fill", d=> colorScale(d.category));
+// Step 5
+var pie = d3.pie().value(function(d) { 
+     return d.amt; 
+ });
 
-    legend.append("text")
-      .attr("x", 15)
-      .attr("y", 9)
-      .attr("dy", "0.35em")
-      .text(d=> d.category);    
+var arc = g.selectAll("arc")
+        .data(pie(data))
+        .enter();
+
+// Step 6
+var path = d3.arc()
+          .outerRadius(radius)
+          .innerRadius(0);
+
+arc.append("path")
+.attr("d", path)
+.attr("fill", function(d) { return ordScale(d.data.category); });
+
+// Step 7
+var label = d3.arc()
+           .outerRadius(radius)
+           .innerRadius(0);
+ 
+arc.append("text")
+.attr("transform", function(d) { 
+         return "translate(" + label.centroid(d) + ")"; 
+ })
+.text(function(d) { return d.data.category; })
+.style("font-family", "arial")
+.style("font-size", 15);
+
     }).catch(error => console.log("Error loading the Pie chart:", error));
 }
 
@@ -114,18 +105,19 @@ function displayBarChart(){
 
   // Load the CSV data using fetch
   d3.json(fraudMultipleURL).then(function(data) {
-    console.log(data)
-    // Extract the "first," "last," and "count" columns
-    var firstNames = data.map(row => row.first + ' ' + row.last);
-    var counts = data.map(row => parseInt(row.count));
-    // Create the bar chart using Chart.js
-    // document.addEventListener ('DOMContentLoaded', function() {
-    // fetchData().then(data => {
-    // initBarChart(data);
-    // });
-    // });
 
-    const ctx = document.getElementById('myChart');
+    // Extract the "first," "last," and "count" columns
+    var firstNames = data.map((currElem, index) => index)//row => row.first + ' ' + row.last);
+    var counts = data.map(row => parseInt(row.count));
+
+    //Select the char container element
+    let barChartContainer = d3.select("#myBarChart");
+
+    //Clear the existing chart
+    barChartContainer.html("");
+
+    const ctx = document.getElementById('myBarChart');
+
     new Chart(ctx, {
       type: 'bar',
       data: {
@@ -133,8 +125,8 @@ function displayBarChart(){
         datasets: [{
           label: 'Count of Multiple Fraud',
           data: counts,
-          backgroundColor: 'rgba(75, 192, 192, 0.6)',
-          borderColor: 'rgba(75, 192, 192, 1)',
+          // backgroundColor: 'rgba(75, 192, 192, 0.6)',
+          // borderColor: 'rgba(75, 192, 192, 1)',
           borderWidth: 1
         }]
       },
@@ -154,29 +146,25 @@ function displayBarChart(){
 // Function to handle dropdown menu selection change event
 function optionChanged(selectedOption) {
   if (selectedOption === "option1") {
-     // Initialize the map if it's not already initialized
-     if (!myMap) {
-      initializeMap();
-    }
     // Display Fraud Density Map
     document.getElementById("map").style.display = "block"; // Show the map
-    document.getElementById("chart-container").style.display = "none"; // Hide the chart
+    document.getElementById("pieChart").style.display = "none"; // Hide the chart
     document.getElementById("barChart").style.display = "none";
     displayFraudDensityMap();
   } else if (selectedOption === "option2") {
     document.getElementById("map").style.display="none"; //Hide the map
-    document.getElementById("chart-container").style.display = "block"; //Show the chart
+    document.getElementById("pieChart").style.display = "block"; //Show the chart
     document.getElementById("barChart").style.display = "none";
     displayPieChart();
   } else if (selectedOption === "option3") {
     document.getElementById("map").style.display="none"; //Hide the map
-    document.getElementById("chart-container").style.display = "none"; //Show the chart
+    document.getElementById("pieChart").style.display = "none"; //Show the chart
     document.getElementById("barChart").style.display = "block";
     displayBarChart();
   } else {
     // Hide the map for other options
     document.getElementById("map").style.display = "none";
-    document.getElementById("chart-container").style.display = "none"; 
+    document.getElementById("pieChart").style.display = "none"; 
     document.getElementById("barChart").style.display = "none";
   }
 }
